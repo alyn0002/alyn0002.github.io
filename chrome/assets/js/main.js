455f54d187147d81862e30f1a649e05292d9b24c
alyn0002 <alyn0002@student.monash.edu>
2021-10-29 11:02:50 +1100

    chrome

--- chrome/assets/js/main.js
+++ chrome/assets/js/main.js
@@ -6,5 +6,31 @@ $(document).ready(function () {
     });
   });
 
+$(document).ready(function() {
+  console.log("ready");
 
- 
+  $('div.lbutton').click(function() {
+      $('div.title').addClass('orange');
+  });
+
+
+$('div.lbutton-font').click(function(){
+    $('div.main2').addClass('sans');
+});
+
+
+$('div.lbutton-day').click(function(){
+  $('div.main2').addClass('day');
+});
+
+
+$('div.lbutton-night').click(function(){
+  $('div.main2').addClass('night');
+});
+
+
+});
+
+
+
+
